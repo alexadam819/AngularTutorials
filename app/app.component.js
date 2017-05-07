d12c780c96eddf998e073dbf31f7398aa74c6bca
Alexander Adam <alexadam@gmail.com>
2017-05-06 22:23:02 -0400

    Progress of the tutorials

--- app/app.component.js
+++ app/app.component.js
@@ -32,11 +32,18 @@ System.register(['angular2/core', './courses.component', './authors.component', 
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.canSave = true;
+                    this.task = {
+                        title: "Review application",
+                        assignee: {
+                            name: "Alex"
+                        }
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <h1>My First Angular 2 App</h1>\n        <courses></courses>\n        <authors></authors>\n        <favorite></favorite>\n        <tweets></tweets>\n        <pipes-demo></pipes-demo>\n    ",
+                        template: "\n        <h1>My First Angular 2 App</h1>\n        <courses></courses>\n        <authors></authors>\n        <favorite></favorite>\n        <tweets></tweets>\n        <pipes-demo></pipes-demo>\n        <h1>Section 5 - 56</h1>\n        <button\n            [ngStyle]=\"{\n                backgroundColor: canSave ? 'blue' : 'gray',\n                color: canSave ? 'white' : 'black',\n                fontWeight: canSave ? 'bold' : 'normal'\n            }\"\n        >Submit</button>\n        <h1>Section 5 - 57</h1>\n        <ul>\n            <li>Title: {{ task.title }}</li>\n            <li *ngIf=\"task.assignee != null\">Assigned to: {{ task.assignee.name }} </li>\n            <li>Assigned to: {{ task.assignee.name != null ? task.assignee.name : \"\" }} </li>\n            <li>This is called Elvis operator</li>\n            <li>Assigned to: {{ task.assignee?.name }} </li>\n        <ul>\n    ",
                         directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent, twitter_component_1.TwitterComponent, pipesdemo_component_1.PipeDemoComponent]
                     }), 
                     __metadata('design:paramtypes', [])
--- app/app.component.ts
+++ app/app.component.ts
@@ -13,7 +13,31 @@ import {PipeDemoComponent} from './pipesdemo.component';
         <favorite></favorite>
         <tweets></tweets>
         <pipes-demo></pipes-demo>
+        <h1>Section 5 - 56</h1>
+        <button
+            [ngStyle]="{
+                backgroundColor: canSave ? 'blue' : 'gray',
+                color: canSave ? 'white' : 'black',
+                fontWeight: canSave ? 'bold' : 'normal'
+            }"
+        >Submit</button>
+        <h1>Section 5 - 57</h1>
+        <ul>
+            <li>Title: {{ task.title }}</li>
+            <li *ngIf="task.assignee != null">Assigned to: {{ task.assignee.name }} </li>
+            <li>Assigned to: {{ task.assignee.name != null ? task.assignee.name : "" }} </li>
+            <li>This is called Elvis operator</li>
+            <li>Assigned to: {{ task.assignee?.name }} </li>
+        <ul>
     `,
     directives: [CoursesComponent, AuthorsComponent, TwitterComponent, PipeDemoComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+    canSave = true;
+    task = {
+        title: "Review application",
+        assignee: {
+            name: "Alex"
+        }
+    }
+}
