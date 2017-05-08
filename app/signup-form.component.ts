82f0fcd4119e4bf13726b0cf8704edc9b977a821
Alexander Adam <alexadam@gmail.com>
2017-05-08 14:10:40 -0400

    Custom Validation Tutorial

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,15 +1,28 @@
 import {Component} from 'angular2/core';
-import {ControlGroup, Control, Validators} from 'angular2/common';
+import {ControlGroup, Control, Validators, FormBuilder} from 'angular2/common';
+import {UsernameValidator} from './usernameValidator'
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
-    form = new ControlGroup({
-        username: new Control('', Validators.required),
-        password: new Control('', Validators.required)
-    });
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            username: ['', Validators.compose([
+                Validators.required, 
+                UsernameValidator.cannotContainSpace
+            ])],
+            password: ['', Validators.required]
+        })
+    }
+
+    // form = new ControlGroup({
+    //     username: new Control('', Valida: tors.required),
+    //     password: new Control('', Validators.required)
+    //});
 
     signup(){
         console.log(this.form.value);
